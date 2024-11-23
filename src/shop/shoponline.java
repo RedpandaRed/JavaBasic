d89dce897f9ea845ecaa6231ac269c575c6179f4
redpanda <dudkevych.kateryna@icloud.com>
2024-11-23 22:59:57 +0200

    shoponline

--- src/shop/shoponline.java
+++ src/shop/shoponline.java
@@ -3,7 +3,7 @@ package shop;
 public class shoponline {
     public static void main(String[] args) {
         System.out.println("Shop-online for customers");
-        System.out.println("Today only, we have 20% off on all products.");
+        System.out.println("Today onl, we have 20% off on all products.");
         int price = 400;
         float discount = 0.2F;
         float discounted_price = price - (discount * price);
